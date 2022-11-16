60516a485dbf8c40e85b7999ee26048be2403bc5
zhagnlu <lu.zhang@zilliz.com>
2022-11-16 19:49:17 +0800

    xx
    Signed-off-by: zhagnlu <lu.zhang@zilliz.com>

--- internal/core/unittest/test_disk_file_manager_test.cpp
+++ internal/core/unittest/test_disk_file_manager_test.cpp
@@ -24,12 +24,28 @@
 #include "config/ConfigChunkManager.h"
 #include "config/ConfigKnowhere.h"
 #include "test_utils/indexbuilder_test_utils.h"
+#include "storage/PayloadReader.h"
 
 using namespace std;
 using namespace milvus;
 using namespace milvus::storage;
 using namespace boost::filesystem;
 using namespace knowhere;
+
+static size_t
+getCurrentRSS() {
+    /* Linux ---------------------------------------------------- */
+    long rss = 0L;
+    FILE* fp = NULL;
+    if ((fp = fopen("/proc/self/statm", "r")) == NULL)
+        return (size_t)0L; /* Can't open? */
+    if (fscanf(fp, "%*s%ld", &rss) != 1) {
+        fclose(fp);
+        return (size_t)0L; /* Can't read? */
+    }
+    fclose(fp);
+    return (size_t)rss * (size_t)sysconf(_SC_PAGESIZE);
+}
 
 class DiskAnnFileManagerTest : public testing::Test {
  public:
@@ -101,8 +117,8 @@ TEST_F(DiskAnnFileManagerTest, AddFilePositive) {
         EXPECT_EQ(rawData[4], data[4]);
     }
 }
-
-TEST_F(DiskAnnFileManagerTest, AddFilePositiveParallel) {
+void
+testtt(StorageConfig& storage_config_) {
     auto& lcm = LocalChunkManager::GetInstance();
     auto rcm = std::make_unique<MinioChunkManager>(storage_config_);
     string testBucketName = "test-diskann";
@@ -114,9 +130,13 @@ TEST_F(DiskAnnFileManagerTest, AddFilePositiveParallel) {
     std::string indexFilePath = "/tmp/diskann/index_files/1000/index";
     auto exist = lcm.Exist(indexFilePath);
     EXPECT_EQ(exist, false);
-    uint64_t index_size = 50 << 20;
+    uint64_t index_size = 300 << 20;
+
     lcm.CreateFile(indexFilePath);
     std::vector<uint8_t> data(index_size);
+    for (int i = 0; i < index_size; ++i) {
+        data[i] = random() % 256;
+    }
     lcm.Write(indexFilePath, data.data(), index_size);
 
     // collection_id: 1, partition_id: 2, segment_id: 3
@@ -138,7 +158,11 @@ TEST_F(DiskAnnFileManagerTest, AddFilePositiveParallel) {
         std::cout << file2size.first << std::endl;
         remote_files.emplace_back(file2size.first);
     }
-    diskAnnFileManager->CacheIndexToDisk(remote_files);
+    for (int i = 0; i < 1000; ++i) {
+        sleep(5);
+        std::cout << " cached done" << std::endl;
+        diskAnnFileManager->CacheIndexToDisk(remote_files);
+    }
     auto local_files = diskAnnFileManager->GetLocalFilePaths();
     for (auto& file : local_files) {
         auto file_size = lcm.Size(file);
@@ -154,6 +178,9 @@ TEST_F(DiskAnnFileManagerTest, AddFilePositiveParallel) {
         EXPECT_EQ(rawData[0], data[0]);
         EXPECT_EQ(rawData[4], data[4]);
     }
+}
+TEST_F(DiskAnnFileManagerTest, AddFilePositiveParallel) {
+    testtt(storage_config_);
 }
 
 int
@@ -201,4 +228,103 @@ TEST_F(DiskAnnFileManagerTest, TestThreadPoolException) {
     } catch (std::exception& e) {
         EXPECT_EQ(std::string(e.what()), "run time error");
     }
+}
+
+void
+test_write(LocalChunkManager* lcm, std::string path, void* data, int size) {
+    lcm->Write(path, data, size);
+}
+
+TEST_F(DiskAnnFileManagerTest, TESTWRITE) {
+    auto& lcm = LocalChunkManager::GetInstance();
+    lcm.CreateFile("/tmp/test.txt");
+    int size = 40 << 20;
+    std::cout << size << std::endl;
+    std::vector<uint8_t> data(size);
+    auto start = chrono::system_clock::now();
+    int batch = size / 10;
+    std::cout << batch << std::endl;
+    for (int i = 0; i < size; i += batch) {
+        lcm.Write("/tmp/test.txt", data.data() + i, batch);
+        std::cout << i << std::endl;
+    }
+    auto end = chrono::system_clock::now();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    std::cout << "duration" << duration << "ms";
+
+    start = chrono::system_clock::now();
+    auto thread_pool = new milvus::ThreadPool(50);
+    std::vector<std::future<void>> futures;
+    for (int i = 0; i < size; i + batch) {
+        futures.push_back(thread_pool->Submit(test_write, &lcm, "/tmp/test.txt", data.data() + i, batch));
+    }
+    for (auto& future : futures) {
+        future.get();
+    }
+    end = chrono::system_clock::now();
+    duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    std::cout << "duration" << duration << "ms";
+}
+
+TEST_F(DiskAnnFileManagerTest, TESTPARQUET) {
+    while (true) {
+        auto mem_pool = arrow::default_memory_pool();
+
+        std::unique_ptr<parquet::arrow::FileReader> reader;
+        auto arrow_memory_mapped_file = arrow::io::MemoryMappedFile::Open("/tmp/a.parquet", arrow::io::FileMode::READ);
+        std::shared_ptr<arrow::io::RandomAccessFile> input = arrow_memory_mapped_file.ValueOrDie();
+        auto st = parquet::arrow::OpenFile(input, mem_pool, &reader);
+        AssertInfo(st.ok(), "failed to get arrow file reader");
+        std::shared_ptr<arrow::Table> table;
+        std::cout << "read before" << getCurrentRSS() << std::endl;
+        st = reader->ReadTable(&table);
+        std::cout << "read after" << getCurrentRSS() << std::endl;
+        AssertInfo(st.ok(), "failed to get reader data to arrow table");
+        auto column = table->column(0);
+        AssertInfo(column != nullptr, "returned arrow column is null");
+        AssertInfo(column->chunks().size() == 1, "arrow chunk size in arrow column should be 1");
+        auto array = column->chunk(0);
+        AssertInfo(array != nullptr, "empty arrow array of PayloadReader");
+        // sleep(1);
+        std::cout << "read sleep" << getCurrentRSS() << std::endl;
+    }
+}
+
+TEST_F(DiskAnnFileManagerTest, TESTPARQUET111) {
+    while (true) {
+        auto mem_pool = arrow::default_memory_pool();
+
+        std::unique_ptr<parquet::arrow::FileReader> reader;
+        auto arrow_memory_mapped_file = arrow::io::MemoryMappedFile::Open("/tmp/a.parquet", arrow::io::FileMode::READ);
+        std::shared_ptr<arrow::io::RandomAccessFile> input = arrow_memory_mapped_file.ValueOrDie();
+        auto st = parquet::arrow::OpenFile(input, mem_pool, &reader);
+        AssertInfo(st.ok(), "failed to get arrow file reader");
+        std::shared_ptr<arrow::Table> table;
+        std::cout << "read before " << getCurrentRSS() << std::endl;
+        std::shared_ptr<arrow::ChunkedArray> array;
+        st = reader->ReadColumn(0, &array);
+        AssertInfo(array != nullptr, "empty arrow array of PayloadReader");
+        auto array1 = array->chunk(0);
+        // sleep(4);
+        std::cout << "read after " << getCurrentRSS() << std::endl;
+    }
+}
+
+class A {
+ public:
+    A() {
+    }
+    ~A() {
+        sleep(10);
+    }
+};
+
+void
+TT() {
+    auto a = std::make_unique<A>();
+}
+
+TEST_F(DiskAnnFileManagerTest, TESTAA) {
+    TT();
+    std::cout << "done" << std::endl;
 }
