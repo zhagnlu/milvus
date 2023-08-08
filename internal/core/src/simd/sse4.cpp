cfdbd3bc6b49d3d0c7877c5566d28fc892b3120e
luzhang <luzhang@zilliz.com>
2023-08-08 12:56:29 +0000

    Add compare simd manual functions
    Signed-off-by: luzhang <luzhang@zilliz.com>

--- internal/core/src/simd/sse4.cpp
+++ internal/core/src/simd/sse4.cpp
@@ -32,9 +32,9 @@ FindTermSSE4(const int64_t* src, size_t vec_size, int64_t val) {
     size_t remaining_size = vec_size % 2;
 
     __m128i xmm_target = _mm_set1_epi64x(val);
-    for (size_t i = 0; i < num_chunk; ++i) {
+    for (size_t i = 0; i < num_chunk * 2; i += 2) {
         __m128i xmm_data =
-            _mm_loadu_si128(reinterpret_cast<const __m128i*>(src + i * 2));
+            _mm_loadu_si128(reinterpret_cast<const __m128i*>(src + i));
         __m128i xmm_match = _mm_cmpeq_epi64(xmm_data, xmm_target);
         int mask = _mm_movemask_epi8(xmm_match);
         if (mask != 0) {
@@ -102,6 +102,47 @@ StrCmpSSE4(const char* s1, const char* s2) {
         }
     }
     return 0;
+}
+
+template <>
+void
+EqualValSSE4(const int8_t* src, size_t size, int8_t val, bool* res) {
+    int num_chunk = size / 16;
+    __m128i xmm_val = _mm_set1_epi8(val);
+
+    for (size_t i = 0; i < 16 * num_chunk; i += 16) {
+        __m128i xmm_src = _mm_loadu_si128((__m128i*)(src + i));
+
+        __m128i xmm_cmp = _mm_cmpeq_epi8(xmm_src, xmm_val);
+
+        _mm_storeu_si128((__m128i*)(res + i), xmm_cmp);
+    }
+
+    for (size_t i = 16 * num_chunk; i < size; ++i) {
+        res[i] = src[i] == val;
+    }
+}
+
+template <>
+void
+EqualValSSE4(const int32_t* src, size_t size, int32_t val, bool* res) {
+    __m128i xmm_val = _mm_set1_epi32(val);
+
+    int middle = size / 4 * 4;
+    for (size_t i = 0; i < middle; i += 4) {
+        __m128i xmm_src = _mm_loadu_si128((__m128i*)(src + i));
+
+        __m128i xmm_cmp = _mm_cmpeq_epi32(xmm_src, xmm_val);
+
+        __m128i xmm_result =
+            _mm_shuffle_epi32(xmm_cmp, _MM_SHUFFLE(3, 2, 1, 0));
+
+        _mm_storeu_si64(res + i, xmm_result);
+    }
+
+    for (size_t i = middle; i < size; ++i) {
+        res[i] = src[i] == val;
+    }
 }
 
 }  // namespace simd
